ff2687d825a95baf31185a0c2d532a6ad438d5ab
10nry <jason.colby@knoxschools.org>
2017-11-29 14:10:09 -0500

    Initial files

---
+++ js/data.js
@@ -0,0 +1,82 @@
+const cardData = [{
+    name: "A.G. Bell",
+    image: "a_g_bell.jpg",
+    id: "agbell"
+  },
+  {
+    name: "Alan Turing",
+    image: "Alan_Turing.jpg",
+    id: "turing"
+  },
+  {
+    name: "Ben Franklin",
+    image: "ben_franklin.jpg",
+    id: "bfranklin"
+  },
+  {
+    name: "Marie Curie",
+    image: "marie-curie.jpg",
+    id: "mcurie"
+  },
+  {
+    name: "Michael Faraday",
+    image: "michael-faraday.jpg",
+    id: "mfaraday"
+  },
+  {
+    name: "Samuel Morse",
+    image: "samuel-morse.jpg",
+    id: "smorse"
+  },
+  {
+    name: "Nikola Tesla",
+    image: "tesla.jpg",
+    id: "tesla"
+  },
+  {
+    name: "Thomas Edison",
+    image: "thomas-edison.jpg",
+    id: "tedison"
+  },
+  {
+    name: "Volta",
+    image: "volta.jpg",
+    id: "volta"
+  },
+  {
+    name: "Wright Brothers",
+    image: "wright_bros.jpg",
+    id: "wbrothers"
+  },
+  {
+    name: "Hedy Lamar",
+    image: "hedy-lamar.gif",
+    id: "hlamar"
+  },
+  {
+    name: "Grace Hopper",
+    image: "grace_hopper.jpg",
+    id: "egg"
+  }
+];
+
+const gameLevels = {
+  easy: {
+    class: "easy",
+    pairs: 6,
+    twoStar: 6,
+    oneStar: 10
+  },
+  medium: {
+    class: "medium",
+    pairs: 8,
+    twoStar: 10,
+    oneStar: 14
+  },
+  hard: {
+    class: "hard",
+    pairs: 12,
+    twoStar: 16,
+    oneStar: 24
+  }
+};
